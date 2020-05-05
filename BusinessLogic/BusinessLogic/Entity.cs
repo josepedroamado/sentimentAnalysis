ed64236d439c27e37cbddd80fc110647c4944db2
PC-PC\PC <frandlnieves@gmail.com>
2020-05-05 18:47:46 -0300

    SentimentTest class created and created a newEntityTest, then ccreated the Entity class and its constructor

---
+++ BusinessLogic/BusinessLogic/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class Entity
+    {
+        public string Name { get; set; }
+        private List<Sentiment> Sentiments;
+        public Entity(String aName)
+        {
+            this.Name = aName;
+            this.Sentiments = new List<Sentiment>();
+        }
+    }
+}
---
+++ BusinessLogic/BusinessLogicTest/EntityTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessLogic;
+
+namespace BusinessLogicTest
+{
+    [TestClass]
+    public class EntityTest
+    {
+        [TestMethod]
+        public void NewEntityTest()
+        {
+            Entity aEntity = new Entity("AName");
+            Assert.IsNotNull(aEntity);
+        }
+    }
+}
