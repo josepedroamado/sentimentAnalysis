6745b49c037bea4aad3f264466179b054c454b14
Jose Pedro Amado <jose.pedro.amado@gmail.com>
2020-05-06 00:12:16 -0300

    First PublicationTest class was created with an initial test constructor method. Then Publication class was added and the constructor was implemented with basic function. Later more test methods were added and changes to Publication class were made.

---
+++ BusinessLogic/BusinessLogic/Publication.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class Publication
+    {
+        public string Phrase { get; set; }
+
+        public DateTime Date { get; set; }
+    
+        public int Id { get; set; }
+
+        public Publication(string aPhrase, DateTime aDate, int anId)
+        {
+            this.Phrase = aPhrase;
+            this.Date = aDate;
+            this.Id = anId;
+        }
+    }
+}
---
+++ BusinessLogic/BusinessLogicTest/PublicationTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessLogic;
+
+namespace BusinessLogicTest
+{
+    [TestClass]
+    public class PublicationTest
+    {
+        [TestMethod]
+        public void NewPublicationTest()
+        {
+            DateTime aDate = new DateTime(2020, 01, 01);
+            Publication aPublication = new Publication("APhrase", aDate, 1);
+            Assert.IsNotNull(aPublication);
+        }
+
+        [TestMethod]
+        public void NewPublicationPhraseTest()
+        {
+            DateTime aDate = new DateTime(2020, 01, 01);
+            Publication aPublication = new Publication("APhrase", aDate, 1);
+            Assert.AreEqual("APhrase", aPublication.Phrase);
+        }
+
+        [TestMethod]
+        public void NewPublicationDateTest()
+        {
+            DateTime aDate = new DateTime(2020, 01, 01);
+            Publication aPublication = new Publication("APhrase", aDate, 1);
+            Assert.AreEqual(aDate, aPublication.Date);
+        }
+
+        [TestMethod]
+        public void NewPublicationIdTest()
+        {
+            DateTime aDate = new DateTime(2020, 01, 01);
+            Publication aPublication = new Publication("APhrase", aDate, 1);
+            Assert.AreEqual(1, aPublication.Id);
+        }
+    }
+}
