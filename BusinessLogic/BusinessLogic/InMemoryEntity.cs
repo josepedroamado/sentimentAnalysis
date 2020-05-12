130dfeed7911c5a6080925b66e8e63f72e76e4bf
PC-PC\PC <frandlnieves@gmail.com>
2020-05-12 19:44:19 -0300

    Worked on the InMemoryEntity methods and tests

---
+++ BusinessLogic/BusinessLogic/InMemoryEntity.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class InMemoryEntity : IEntitySaver
+    {
+        private List<Entity> Entities;
+
+        public InMemoryEntity()
+        {
+            this.Entities = new List<Entity>();
+        }
+        public void AddEntity(Entity aEntity)
+        {
+            if(this.EntityExists(aEntity.Name))
+            {
+                throw new EntityAlreadyExistsException();
+            }
+            this.Entities.Add(aEntity);
+        }
+
+        public void DeleteEntity(string aName)
+        {
+            if (!this.EntityExists(aName))
+            {
+                throw new EntityDoesntExistException();
+            }
+            for (int i = 0; i < this.Entities.Count(); i++)
+            {
+                if (Entities[i].Name == aName)
+                {
+                    Entities.RemoveAt(i);
+                }
+            }
+            
+        }
+
+        public Entity FetchEntity(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ModifyEntity(Entity aEntity)
+        {
+            throw new NotImplementedException();
+        }
+        public Boolean EntityExists(string aName)
+        {
+            for(int i = 0; i < this.Entities.Count(); i++)
+            {
+                if(Entities[i].Name == aName)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ BusinessLogic/BusinessLogicTest/InMemoryEntityTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessLogic;
+
+namespace BusinessLogicTest
+{
+    [TestClass]
+    public class InMemoryEntityTest
+    {
+        [TestMethod]
+        public void CreateInMemoryEntityTest()
+        {
+            IEntitySaver aEntitySaver = new InMemoryEntity();
+            Assert.IsNotNull(aEntitySaver);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(EntityAlreadyExistsException))]
+        public void EntityExistsTest()
+        {
+            IEntitySaver aEntitySaver = new InMemoryEntity();
+            Entity aEntity = new Entity("aText");
+            aEntitySaver.AddEntity(aEntity);
+            aEntitySaver.AddEntity(aEntity);
+            Assert.IsNotNull(aEntitySaver);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(EntityDoesntExistException))]
+        public void EntityDeletetTest()
+        {
+            IEntitySaver aEntitySaver = new InMemoryEntity();
+            aEntitySaver.DeleteEntity("aText");            
+        }
+    }
+}
