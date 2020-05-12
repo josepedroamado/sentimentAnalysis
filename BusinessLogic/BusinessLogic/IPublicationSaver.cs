c085c686c78254b679d7847dfb8cef9549fb82c3
PC-PC\PC <frandlnieves@gmail.com>
2020-05-12 18:55:53 -0300

    Systema class created and some of the interfaces it uses

---
+++ BusinessLogic/BusinessLogic/IPublicationSaver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public interface IPublicationSaver
+    {
+        void AddPublication(Entity aPublication);
+
+        void DeletePublication(int id);
+
+        void ModifyPublication(Publication aPublication);
+
+        Entity FetchPublication(int id);
+    }
+}
