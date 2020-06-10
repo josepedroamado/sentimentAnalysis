47d5f769fb512693627a64f4abd6f6db9b8a51dc
PC-PC\PC <frandlnieves@gmail.com>
2020-06-10 13:09:21 -0300

    Started implementing and testing Author

---
+++ BusinessLogic/BusinessLogicTest/AuthorTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessLogic;
+
+namespace BusinessLogicTest
+{
+    [TestClass]
+    public class AuthorTest
+    {
+        string name;
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            
+        }
+        [TestMethod]
+        public void AuthorNameOkayTest()
+        {
+
+        }
+    }
+}
