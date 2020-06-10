e198c6f7a6c25828e79f52c194f97e0b28d60d2f
PC-PC\PC <frandlnieves@gmail.com>
2020-06-10 12:57:05 -0300

    Author created

---
+++ BusinessLogic/BusinessLogic/Exceptions/AgeTooLongException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace BusinessLogic
+{
+    [Serializable]
+    internal class AgeTooLongException : Exception
+    {
+
+
+        public AgeTooLongException(int age)
+            : base(String.Format("La Edad no puede ser mayor a : " + age))
+        {
+
+        }
+
+
+
+
+    }
+}
---
+++ BusinessLogic/BusinessLogic/Exceptions/AgeTooShortException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace BusinessLogic
+{
+    [Serializable]
+    public class AgeTooShortException : Exception
+    {
+
+
+        public AgeTooShortException(int age)
+            : base(String.Format("La Edad no puede ser menor a : " + age))
+        {
+
+        }
+
+
+
+
+    }
+}
