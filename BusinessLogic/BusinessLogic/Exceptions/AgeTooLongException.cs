4a2111b685a8b18beab8aa2c820907009dea487d
PC-PC\PC <frandlnieves@gmail.com>
2020-06-20 12:07:49 -0300

    Finished author testing

--- BusinessLogic/BusinessLogic/Exceptions/AgeTooLongException.cs
+++ BusinessLogic/BusinessLogic/Exceptions/AgeTooLongException.cs
@@ -4,7 +4,7 @@ using System.Runtime.Serialization;
 namespace BusinessLogic
 {
     [Serializable]
-    internal class AgeTooLongException : Exception
+    public class AgeTooLongException : Exception
     {
 
 
--- BusinessLogic/BusinessLogicTest/AuthorTest.cs
+++ BusinessLogic/BusinessLogicTest/AuthorTest.cs
@@ -7,16 +7,56 @@ namespace BusinessLogicTest
     [TestClass]
     public class AuthorTest
     {
-        string name;
+
+        DateTime ofAgeDate;
         [TestInitialize]
         public void TestInitialize()
         {
-            
+            ofAgeDate = new DateTime(1997, 6, 7);
         }
         [TestMethod]
-        public void AuthorNameOkayTest()
+        public void AuthorIsNotNullTest()
         {
-
+            Author actualAuthor = new Author("James45", "James", "Doe", ofAgeDate);
+            Assert.IsNotNull(actualAuthor);
+        }
+        [TestMethod]
+        public void AuthorIsUserNameOkTest()
+        {
+            Author actualAuthor = new Author("James45", "James", "Doe", ofAgeDate);
+            Assert.AreEqual(actualAuthor.UserName,"James45");
+        }
+        [TestMethod]
+        public void AuthorIsNameOkTest()
+        {
+            Author actualAuthor = new Author("James45", "James", "Doe", ofAgeDate);
+            Assert.AreEqual(actualAuthor.Name, "James");
+        }
+        [TestMethod]
+        public void AuthorIsLastNameOkTest()
+        {
+            Author actualAuthor = new Author("James45", "James", "Doe", ofAgeDate);
+            Assert.AreEqual(actualAuthor.LastName, "Doe");
+        }
+        [TestMethod]
+        public void AuthorIsAgeEqualTest()
+        {
+            Author actualAuthor = new Author("James45", "James", "Doe", ofAgeDate);
+            Assert.AreEqual(actualAuthor.BirthDate, ofAgeDate);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(AgeTooShortException))]
+        public void AuthorIsNotOfAgeTest()
+        {
+            DateTime notOfAge = new DateTime(2012, 6, 7);
+            Author actualAuthor = new Author("James45", "James", "Doe", notOfAge);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(AgeTooLongException))]
+        public void AuthorIsOverAgeTest()
+        {
+            DateTime notOfAge = new DateTime(1912, 6, 7);
+            Author actualAuthor = new Author("James45", "James", "Doe", notOfAge);
         }
     }
 }
